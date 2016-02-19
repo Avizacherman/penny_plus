86d24a712a1873b33d4bf4313803b494b5c136b1
Avi Zacherman <avizacherman@gmail.com>
2016-02-19 16:14:28 -0500

    All implemented tests passing

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,14 @@
 class Api::UsersController < ApplicationController
 
   def index
-    @users = User.all
-    render formats: :json
+    distance = params[:distance] || 1
+
+    if(cookies.signed[:user_id])
+      current_user = User.find(cookies.signed[:user_id])
+      @users = User.joins(:geodatum).within(distance, origin: [current_user.geodatum.lat, current_user.geodatum.lng])
+      render formats: :json
+    else
+      render json: {error: "Not logged in"}
+    end
   end
 end
--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 RSpec.describe Api::UsersController, type: :controller do
   render_views
-  
+
   let :json do JSON.parse response.body end
 
   context "Index Route" do
@@ -13,13 +13,19 @@ RSpec.describe Api::UsersController, type: :controller do
 
       LAT_MINUTES_TO_DECIMAL_RATE = 0.0166666666667
 
+      10.times do |i|
+        user = User.new({first_name: Faker::Name.first_name, last_name: Faker::Name.last_name})
+        user.save(validate: false)
+        user.geodatum.update({ lat: base_lat + (i * LAT_MINUTES_TO_DECIMAL_RATE * 0.001), lng: base_lng})
+      end
+
+
       140.times do |i|
         user = User.new({first_name: Faker::Name.first_name, last_name: Faker::Name.last_name})
         user.save(validate: false)
-        user.geodatum.update({ lat: base_lat + (i * LAT_MINUTES_TO_DECIMAL_RATE / 2).floor, lng: base_lng})
+        user.geodatum.update({ lat: base_lat + (i * LAT_MINUTES_TO_DECIMAL_RATE), lng: base_lng})
       end
 
-      cookies.signed[:user_id] = User.first.id
 
 
     end
@@ -27,12 +33,24 @@ RSpec.describe Api::UsersController, type: :controller do
 
 
     it "By default should respond with data within 1 mile of the users last known location" do
+      cookies.signed[:user_id] = User.first.id
+
       get :index
-      expect(json["users"].count).to eq 20
+      expect(json["users"].count).to eq 11
     end
 
     it "Should allow for expansion of the radius if given a distance value" do
+      cookies.signed[:user_id] = User.first.id
+      DISTANCE_MINUTES_TO_MILES_LAT = 1.15
+      distance = 20 * DISTANCE_MINUTES_TO_MILES_LAT
+      get :index, distance: distance
+      expect(json["users"].count).to eq 30
+    end
 
+    it "Should give an error if there is no logged in user" do
+      get :index
+      err_response = {"error": "Not logged in"}.to_json
+      expect(response.body).to eq err_response
     end
   end
 end
